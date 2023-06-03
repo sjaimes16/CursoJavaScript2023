542361a7e99dc272b40b642dae4db6b4e53bc2dd
yurley <yurs.ksf1@gmail.com>
2023-06-02 20:09:56 -0500

    add example circulo

---
+++ script.js
@@ -0,0 +1,9 @@
+// calcular el area de un circulo
+// Formula: Area = PI * Radio (al cuadrado)
+// Formula: Area = PI * Radio * Radio
+
+PI = 3.1416
+radio = 5
+
+area = PI*radio*radio
+console.log('area: ', area)
