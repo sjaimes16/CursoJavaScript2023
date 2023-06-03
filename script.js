48f1153662d319bf4993bd48ce26b606e9d9f5a5
yurley <yurs.ksf1@gmail.com>
2023-06-02 20:13:14 -0500

    add example circulo

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ radio = 5
 
 area = PI*radio*radio
 console.log('area: ', area)
+
+// Ejercicio. 
+// 1) Calcular el area de la figura geometrica, Rectangulo.
+// con dimenciones de:
+// base = 30 , altura 100
+// nota: el area de un rectangulo es de base por altura. 
+
+
+// 2) Calcular el area de la figura geometrica, Triangulo.
+// con dimenciones de:
+// base = 60 , altura 120
+// nota: el area de un trinagulo es la mitad de de base por altura. 
+
+
+// 2) Calcular el area de la figura geometrica, cuadrado.
+// con dimenciones de:
+// lado = 8 
+// nota: el area de un cuadrado es lado por lado.
