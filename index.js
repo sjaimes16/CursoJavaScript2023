dc27e78267e7b6181a76d83cdf4e2b58facb1287
Sergio <sjaimes16@outlook.com>
2023-06-04 10:55:37 -0500

    agregado commit de ejemplo

---
+++ index.js
@@ -0,0 +1,13 @@
+let base = 30;
+let altura = 100;
+ area = base * altura;
+ console.log("area", area)
+
+ let baseTriangulo = 60;
+ let alturaTriangulo = 120;
+ triangulo = (base * altura) / 2;
+ console.log("triangulo", triangulo)
+
+ let lado = 8;
+ cuadrado = lado * lado;
+ console.log("cuadrado", cuadrado);
